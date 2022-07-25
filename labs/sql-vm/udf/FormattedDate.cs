6629b897ee425c7b4424238cedfd9af494a1908e
Elton Stoneman <elton@sixeyed.com>
2022-07-25 13:15:52 +0100

    Replace UDF

--- labs/sql-vm/udf/FormattedDate.cs
+++ labs/sql-vm/udf/FormattedDate.cs
@@ -1,18 +1,9 @@
-using Microsoft.SqlServer.Server; 
 using System; 
-using System.Data.SqlClient;  
   
 public class FormattedDate
 {  
-    [SqlFunction(DataAccess = DataAccessKind.Read)]  
-    public static string LegacySystem()  
-    {  
-        using (var conn = new SqlConnection("context connection=true"))  
-        {  
-            conn.Open();  
-            var cmd = new SqlCommand("SELECT GETDATE()", conn);  
-            var now = (DateTime)cmd.ExecuteScalar();  
-            return now.ToString("1yyMMdd");
-        }  
+    public static string LegacyNow()  
+    { 
+        return DateTime.Now.ToString("1yyMMdd");
     }  
 }
