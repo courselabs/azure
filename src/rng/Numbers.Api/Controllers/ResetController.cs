67380fe708f988c8e48f4b3e4b539f3895af9412
Elton Stoneman <elton@sixeyed.com>
2022-08-08 15:58:14 +0100

    RNG app source

---
+++ src/rng/Numbers.Api/Controllers/ResetController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Numbers.Api.Controllers
+{
+    [ApiController]
+    [Route("reset")]
+    public class ResetController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            Status.Healthy = true;
+            return Ok("Ok");
+        }
+    }
+}
---
+++ src/rng/Numbers.Api/Controllers/RngController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Numbers.Api.Controllers
+{
+    [ApiController]
+    [Route("rng")]
+    public class RngController : ControllerBase
+    {
+        private static Random _Random = new Random();
+        private static int _CallCount;
+
+        private readonly ILogger<RngController> _logger;
+        private readonly IConfiguration _config;
+        private readonly int _failAfterCallCount;
+        private readonly bool _useFailureId;
+        private readonly string _instance;
+
+        public RngController(IConfiguration config, ILogger<RngController> logger)
+        {
+            _config = config;
+            _logger = logger;
+            _failAfterCallCount = _config.GetValue<int>("FailAfterCallCount");
+            _useFailureId = _config.GetValue<bool>("UseFailureId");
+            _instance = Dns.GetHostName();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            _CallCount++;
+            
+            if (Status.Healthy)
+            {
+                var n = _Random.Next(0, 100);
+                _logger.LogDebug($"Instance: {_instance}. Returning random number: {n}");
+
+                if (_failAfterCallCount > 0 && _CallCount >= _failAfterCallCount)
+                {
+                    Status.Healthy = false;
+                }
+
+                return Ok(n);
+            }
+            else
+            {
+                var message = _useFailureId ? $"Instance: {_instance}. Unhealthy! Failure ID: {Guid.NewGuid()}" : "Instance: {_instance}. Unhealthy!";
+                _logger.LogWarning(message);
+                return StatusCode(500, new { message= message });
+            }
+        }
+    }
+}
